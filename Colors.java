8258ea128a1b3e425da195e392957b9f099086a4
Gage <a31gage@siena.edu>
2019-04-12 14:32:34 -0400

    Enum that helps with colors of paths

---
+++ Colors.java
@@ -0,0 +1,12 @@
+
+/**
+ * Enumeration class Colors - write a description of the enum class here
+ *
+ * @author Austin Gage, Derek McPhail, Cheryl McClean,
+ * Rose Wilson, Mark Eliseo
+ * @version 4/12/2019
+ */
+public enum Colors
+{
+    BLACK, BLUE, RED, RAINBOW, GREEN, ORANGE, PINK, NONE
+}
