7e43a5883c2446506ad0701db8b393f6e08ff8ea
DMcPhail15 <dt06mcph@siena.edu>
2019-04-17 11:10:15 -0400

    Updated

--- DestCard.java
+++ DestCard.java
@@ -1,4 +1,13 @@
-
+import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.*;
+import java.util.Random;
 /**
  * instances of images of card
  * to and from
@@ -6,15 +15,49 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class DestCard extends Tickets
+public class DestCard
 {
+    protected ArrayList<Image> destCards;
+    private Toolkit toolkit = Toolkit.getDefaultToolkit();
     /**
      * Constructor for objects of class DestCard
      */
     public DestCard()
     {
-        destCards.shuffle();
+        Path path = Paths.get("fwdboardandtransport");        
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(path))
+        {
+            for(Path file: stream)
+            {
+                if (file.toString().contains("to"))
+                {
+                    destCards.add(toolkit.getImage(file.toString()));
+                }
+
+            }
+        }
+        catch(Exception e)
+        {
+            System.exit(0);
+        }
+        shuffle();
     }
 
-    public boolean 
+    /**
+     * Shuffles the deck of destination cards in a pseudorandom order.
+     * 
+     * @author names
+     * @version date
+     */
+    public void shuffle() {
+        for (int i = 0; i < destCards.size(); i++) {
+            Random r = new Random();
+            int randomIndex = r.nextInt(destCards.size());
+            Image temp = destCards.get(i);
+            destCards.set(i, destCards.get(randomIndex));
+            destCards.set(randomIndex, temp);
+        }
+    }
+
+    
 }
