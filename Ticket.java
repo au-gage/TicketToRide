a5d0cdd6c1865e102cead5a060bc0be1a963761d
Gage <a31gage@siena.edu>
2019-04-24 10:52:07 -0400

    Ticket class

---
+++ Ticket.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.*;
+
+/**
+ * Write a description of class Ticket here.
+ *
+ * @author Austin Gage
+ * @version 4/23/2019
+ */
+public class Ticket
+{
+    private Image image;
+    private Colors color;
+    
+    public Ticket(Image image, Colors color)
+    {
+        this.image = image;
+        this.color = color;
+    }
+    
+    public Image getImage()
+    {
+        return image;
+    }
+    
+    public Colors color()
+    {
+        return color;
+    }
+}
