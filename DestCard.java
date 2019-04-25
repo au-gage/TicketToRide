94b5298a86e5d3d20e8ee25829947449c61e8847
DMcPhail15 <dt06mcph@siena.edu>
2019-04-25 12:19:47 -0400

    Finish DestCard value method

--- DestCard.java
+++ DestCard.java
@@ -42,65 +42,197 @@ public class DestCard
     /**
      * Returns the value of a destination card 
      */
-    public static int destCardValues(DestCard card){
+    public static int destCardValues(Edges route, DestCard card){
         int value = 0; 
-        if(card.getImage().toString() == "CPtoC"){
+        
+        if((route.getIsCaptured("Central Park", "Chelsea") || route.getIsCaptured("Chelsea", "Central Park")) && card.getImage().toString() == "CPtoC"){
             value = 5;
-        }   
-        else if(card.getImage().toString() == "CPtoCT"){
-            value = 8;
+            return value;
+        } 
+        else if((!route.getIsCaptured("Central Park", "Chelsea") || !route.getIsCaptured("Chelsea", "Central Park")) && card.getImage().toString() == "CPtoC"){
+            value = -5;
+            return value;
         }
-        else if(card.getImage().toString() == "CPtoGP"){
+        
+        if((route.getIsCaptured("Central Park", "Chinatown") || route.getIsCaptured("Chinatown", "Central Park")) && card.getImage().toString() == "CPtoCT"){
+            value = 8;
+            return value;
+        }
+        else if((!route.getIsCaptured("Central Park", "Chinatown") || !route.getIsCaptured("Chinatown", "Central Park")) && card.getImage().toString() == "CPtoCT"){
+            value = -8;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Central Park", "Gramercy Park") || route.getIsCaptured("Gramercy Park", "Central Park")) && card.getImage().toString() == "CPtoGP"){
             value = 4;
+            return value;
         }
-        else if(card.getImage().toString() == "CPtoMW"){
+        else if((!route.getIsCaptured("Central Park", "Gramercy Park") || !route.getIsCaptured("Gramercy Park", "Central Park")) && card.getImage().toString() ==
+        "CPtoGP"){
+            value = -4;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Central Park", "Midtown West") || route.getIsCaptured("Midtown West", "Central Park"))  && card.getImage().toString() == "CPtoMW"){
             value = 3;
+            return value;
         }
-        else if(card.getImage().toString() == "CtoB"){
+        else if((!route.getIsCaptured("Central Park", "Midtown West") || !route.getIsCaptured("Midtown West", "Central Park")) && card.getImage().toString() ==
+        "CPtoMW"){
+            value = -3;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Chelsea", "Brooklyn") || route.getIsCaptured("Brooklyn", "Chelsea"))  && card.getImage().toString() == "CtoB"){
             value = 8;
+            return value;
         }
-        else if(card.getImage().toString() == "CtoWS"){
+        else if((!route.getIsCaptured("Chelsea", "Brooklyn") || !route.getIsCaptured("Brooklyn", "Chelsea")) && card.getImage().toString() == "CtoB"){
+            value = -8;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Chelsea", "Wall Street") || route.getIsCaptured("Wall Street", "Chelsea")) && card.getImage().toString() == "CtoWS"){
             value = 8;
+            return value;
         }
-        else if(card.getImage().toString() == "ESBtoB"){
+        else if((!route.getIsCaptured("Chelsea", "Wall Street") || route.getIsCaptured("Wall Street", "Chelsea")) && card.getImage().toString() == "CtoWS"){
+            value = -8;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Empire State Building", "Brooklyn") || route.getIsCaptured("Brooklyn", "Empire State Building")) && card.getImage().toString() == 
+        "ESBtoB"){
             value = 6;
+            return value;
         }
-        else if(card.getImage().toString() == "ESBtoGV"){
+        else if((!route.getIsCaptured("Empire State Building", "Brooklyn") || !route.getIsCaptured("Brooklyn", "Empire State Building")) 
+        && card.getImage().toString() == "ESBtoB"){
+            value = -6;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Empire State Building", "Greenwich Village") || route.getIsCaptured("Greenwich Village", "Empire State Building")) && 
+        card.getImage().toString() == "ESBtoGV"){
             value = 3;
+            return value;
         }
-        else if(card.getImage().toString() == "EVtoS"){
+        else if((!route.getIsCaptured("Empire State Building", "Greenwich Village") || !route.getIsCaptured("Greenwich Village", "Empire State Building")) && 
+        card.getImage().toString() == "ESBtoGV"){
+            value = -3;
+            return value;
+        }
+        
+        if((route.getIsCaptured("East Village", "Soho") || route.getIsCaptured("Soho", "East Village")) && card.getImage().toString() == "EVtoS"){
             value = 4;
+            return value;
         }
-        else if(card.getImage().toString() == "GPtoCT"){
+        else if((!route.getIsCaptured("East Village", "Soho") || !route.getIsCaptured("Soho", "East Village")) && card.getImage().toString() == "EVtoS"){
+            value = -4;
+            return value;
+        }
+       
+        if((route.getIsCaptured("Gramercy Park", "Chinatown") || route.getIsCaptured("Chinatown", "Gramercy Park")) && card.getImage().toString() == "GPtoCT"){
             value = 4;
+            return value;
         }
-        else if(card.getImage().toString() == "LCtoESB"){
+        else if((!route.getIsCaptured("Gramercy Park", "Chinatown") || !route.getIsCaptured("Chinatown", "Gramercy Park")) && card.getImage().toString() 
+        == "GPtoCT"){
+            value = -4;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Lincoln Center", "Empire State Building") || route.getIsCaptured("Empire State Building", "Lincoln Center")) 
+        && card.getImage().toString() == "LCtoESB"){
             value = 3;
+            return value;
         }
-        else if(card.getImage().toString() == "LCtoGV"){
+        else if ((!route.getIsCaptured("Lincoln Center", "Empire State Building") || !route.getIsCaptured("Empire State Building", "Lincoln Center")) 
+        && card.getImage().toString() == "LCtoESB"){
+            value = -3;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Lincoln Center", "Greenwich Village") || route.getIsCaptured("Greenwich Village", "Lincoln Center")) 
+        && card.getImage().toString() == "LCtoGV"){
             value = 6;
+            return value;
         }
-        else if(card.getImage().toString() == "LEStoWS"){
+        else if((!route.getIsCaptured("Lincoln Center", "Greenwich Village") || !route.getIsCaptured("Greenwich Village", "Lincoln Center")) 
+        && card.getImage().toString() == "LCtoGV"){
+            value = -6;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Lower East Side", "Wall Street") || route.getIsCaptured("Wall Street", "Lower East Side")) 
+        && card.getImage().toString() == "LEStoWS"){
             value = 2;
+            return value;
         }
-        else if(card.getImage().toString() == "TStoB"){
+        else if((!route.getIsCaptured("Lower East Side", "Wall Street") || !route.getIsCaptured("Wall Street", "Lower East Side"))
+        && card.getImage().toString() == "LEStoWS"){
+            value = -2;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Times Square", "Brooklyn") || route.getIsCaptured("Brooklyn", "Times Square")) 
+        && card.getImage().toString() == "TStoB"){
             value = 8;
+            return value;
         }
-        else if(card.getImage().toString() == "TStoEV"){
+        else if((!route.getIsCaptured("Times Square", "Brooklyn") || !route.getIsCaptured("Brooklyn", "Times Square")) && card.getImage().toString() == "TStoB"){
+            value = -8;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Times Square", "East Village") || route.getIsCaptured("East Village", "Times Square"))
+        && card.getImage().toString() == "TStoEV"){
             value = 4;
+            return value;
         }
-        else if(card.getImage().toString() == "TStoS"){
+        else if((!route.getIsCaptured("Times Square", "East Village") || !route.getIsCaptured("East Village", "Times Square")) 
+        && card.getImage().toString() == "TStoEV"){
+            value = -4;
+            return value;
+        }
+        
+        if((route.getIsCaptured("Times Square", "Soho") || route.getIsCaptured("Soho", "Times Square")) && card.getImage().toString() == "TStoS"){
             value = 6;
+            return value;
         }
-        else if(card.getImage().toString() == "UNtoMW"){
+        else if((!route.getIsCaptured("Times Square", "Soho") || !route.getIsCaptured("Soho", "Times Square")) && card.getImage().toString() == "TStoS"){
+            value = -6;
+            return value;
+        }
+        
+        if((route.getIsCaptured("United Nations", "Midtown West") || route.getIsCaptured("Midtown West", "United Nations")) && card.getImage().toString() == "UNtoMW"
+        ){
             value = 3;
+            return value;
         }
-        else if(card.getImage().toString() == "UNtoWS"){
+        else if((!route.getIsCaptured("United Nations", "Midtown West") || !route.getIsCaptured("Midtown West", "United Nations")) && card.getImage().toString() == 
+        "UNtoMW"){
+            value = -3;
+            return value;
+        }
+        
+        if((route.getIsCaptured("United Nations", "Wall Street") || route.getIsCaptured("Wall Street", "United Nations")) && card.getImage().toString() == "UNtoWS"){
             value = 8;
+            return value;
         }
+        else if((!route.getIsCaptured("United Nations", "Wall Street") || !route.getIsCaptured("Wall Street", "United Nations")) && card.getImage().toString() == 
+        "UNtoWS"){
+            value = -8;
+            return value;
+        }      
 
         return value;
-
     }
+    
 
+    public boolean destCardCompleted(Path route, DestCard card){
+        
+        
+        return false;
+    }
 }
